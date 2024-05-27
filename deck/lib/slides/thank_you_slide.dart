cb6198e4ab9e40351807bd797858e88f4f37cd6f
Dario Digregorio <info@dario-digregorio.de>
2024-05-28 00:06:12 +0200

    Update thank you slide layout and alignment

--- deck/lib/slides/thank_you_slide.dart
+++ deck/lib/slides/thank_you_slide.dart
@@ -16,56 +16,21 @@ class ThankYouSlide extends FlutterDeckSlideWidget {
               padding: const EdgeInsets.all(128),
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Center(
-                      child: Text('Thank You!',
-                          style: Theme.of(context).textTheme.displayLarge),
-                    ),
-                    const SizedBox(height: 50),
-                    Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  'assets/qrcode_linkedIn.png',
-                                  width: 250,
-                                ),
-                                const SizedBox(height: 16),
-                                Text('LinkedIn: Dario Digregorio',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineMedium),
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  'assets/qrcode_yawa.png',
-                                  width: 250,
-                                ),
-                                const SizedBox(height: 16),
-                                Text('My App: Yawa',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineMedium),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    Text('Thank You!',
+                        style: Theme.of(context).textTheme.displayLarge),
                     const SizedBox(height: 64),
-                    Center(
-                      child: Text(
-                          'You can find me on LinkedIn, GitHub, X and Medium!',
-                          style: Theme.of(context).textTheme.headlineMedium),
+                    Image.asset(
+                      'assets/qrcode_linkedIn.png',
+                      width: 250,
                     ),
+                    const SizedBox(height: 16),
+                    Text('Scan to know how to connect with me',
+                        style: Theme.of(context).textTheme.headlineMedium),
+                    const SizedBox(height: 64),
+                    Text('You can find me on LinkedIn, GitHub, X and Medium!',
+                        style: Theme.of(context).textTheme.headlineMedium),
                   ]),
             ));
   }
