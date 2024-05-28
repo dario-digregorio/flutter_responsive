0c1ae47553dfb1d07ba5a442c6ec1010555f5465
Dario Digregorio <info@dario-digregorio.de>
2024-05-28 14:28:10 +0200

    chore: Remove unused import statement in measure_slide.dart

--- deck/lib/slides/measure_slide.dart
+++ deck/lib/slides/measure_slide.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_deck/flutter_deck.dart';
 
 class MeasureSlide extends FlutterDeckSlideWidget {
